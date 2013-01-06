30d93b0e7700cfd1e2351ef1c364adbf56fa7351
miguel <miguel@ubuntu-workspace.(none)>
2013-01-06 16:44:57 +0100

    Change structure of app.php

--- app.php
+++ app.php
@@ -2,7 +2,6 @@
 	error_reporting(E_ALL ^ E_WARNING);
 
 	require_once('bin/common_config.php');
-	
-	$database = new Database;
-	print_r($database->updateMassProxies(-1, -1));
+
+	$database->updateMassProxies(-1, -1);
 ?>
